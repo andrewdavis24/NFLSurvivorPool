9435fa560ae30ef7a85a0e57c86fca3e072e1d79
andrewdavis <davi1457@purdue.edu>
2026-09-11 11:57:58 -0500

    Logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import roomateLogo from './assets/IMG_4016.jpeg'
 import './App.css'
 
 function App() {
@@ -11,9 +12,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={roomateLogo} className="base" width="170" height="179" alt="" />
         </div>
         <div>
           <h1>Get started</h1>
